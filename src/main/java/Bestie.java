6a880e38cb10b3aaa0917e2ce9fbc0851960efc9
Julius Gwee <e1406504@u.nus.edu>
2025-08-30 04:31:00 +0800

    Add delete task function to Bestie

--- src/main/java/Bestie.java
+++ src/main/java/Bestie.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bestie {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // Introduction
         System.out.println("____________________________________________________________\n"
@@ -31,25 +31,43 @@ public class Bestie {
                     case "list":
                         System.out.println("____________________________________________________________");
                         System.out.println(" Here is your task list bestie!");
-                        for (int i = 0; i < taskCount; i++) {
-                            System.out.println(" " + (i + 1) + "." + tasks[i]);
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(" " + (i + 1) + "." + tasks.get(i));
                         }
                         System.out.println("____________________________________________________________");
                         break;
 
                     case "mark":
+                        if (parts.length < 2) {
+                            throw new BestieException("lmk which you want to mark!");
+                        }
                         int markIndex = Integer.parseInt(parts[1]) - 1;
-                        tasks[markIndex].markAsDone();
+                        tasks.get(markIndex).markAsDone();
                         System.out.println("____________________________________________________________\n"
-                                + " YAYYY ive marked: \n" + "  " + tasks[markIndex] + "\n"
+                                + " YAYYY ive marked: \n" + "  " + tasks.get(markIndex) + "\n"
                                 + "____________________________________________________________\n");
                         break;
 
                     case "unmark":
+                        if (parts.length < 2) {
+                            throw new BestieException("lmk which you want to UNmark!");
+                        }
                         int unmarkIndex = Integer.parseInt(parts[1]) - 1;
-                        tasks[unmarkIndex].markAsUndone();
+                        tasks.get(unmarkIndex).markAsUndone();
                         System.out.println("____________________________________________________________\n"
-                                + " no worries! ive unmarked: \n" + "  " + tasks[unmarkIndex] + "\n"
+                                + " no worries! ive unmarked: \n" + "  " + tasks.get(unmarkIndex) + "\n"
+                                + "____________________________________________________________\n");
+                        break;
+
+                    case "delete":
+                        if (parts.length < 2) {
+                            throw new BestieException("lmk which you want to delete!");
+                        }
+                        int delIndex = Integer.parseInt(parts[1]) - 1;
+                        Task removed = tasks.remove(delIndex);
+                        System.out.println("____________________________________________________________\n"
+                                + " okay g ive removed this task: \n" + "  " + removed + "\n"
+                                + " now you have " + tasks.size() + " tasks in your list!!" + "\n"
                                 + "____________________________________________________________\n");
                         break;
 
@@ -57,9 +75,9 @@ public class Bestie {
                         if (parts.length < 2 || parts[1].trim().isEmpty()) {
                             throw new BestieException("why is your todo empty >:(");
                         }
-                        Task todo = new Todo(parts[1]);
-                        tasks[taskCount++] = todo;
-                        printAdd(todo, taskCount);
+                        Task todo = new Todo(parts[1].trim());
+                        tasks.add(todo);
+                        printAdd(todo, tasks.size());
                         break;
 
                     case "deadline":
@@ -71,8 +89,8 @@ public class Bestie {
                             throw new BestieException("give me the task you want to do as well as a date and time!");
                         }
                         Task deadline = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
-                        tasks[taskCount++] = deadline;
-                        printAdd(deadline, taskCount);
+                        tasks.add(deadline);
+                        printAdd(deadline, tasks.size());
                         break;
 
                     case "event":
@@ -86,8 +104,8 @@ public class Bestie {
                             throw new BestieException("PLEASEEE give me and event description and time");
                         }
                         Task event = new Event(descr, fromTo[0].trim(), fromTo[1].trim());
-                        tasks[taskCount++] = event;
-                        printAdd(event, taskCount);
+                        tasks.add(event);
+                        printAdd(event, tasks.size());
                         break;
 
                     default:

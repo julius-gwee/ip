e93a4df683a5a5f074ec677642aca9846999a2dc
Julius Gwee <e1406504@u.nus.edu>
2025-09-16 17:07:19 +0800

    Update Bestie and Command to use Ui, Parser and TaskList.

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,9 +1,9 @@
 public abstract class Command {
     /** Override if this command should exit the app */
-    public boolean isExit {
+    public boolean isExit() {
         return false;
     }
 
     /** Execute the command logic. */
-    public abstract void execute(TaskList tasks, UI ui, Storage storage) throws BestieException;
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws BestieException;
 }
